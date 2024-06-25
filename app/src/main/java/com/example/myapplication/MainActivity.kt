4a442546c97ca4dc045264e5538a675c882ef2bf
andrey . <andreysyubaev68@gmail.com>
2024-06-26 02:56:06 +0500

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+
+        binding.bFrag2.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.place_holder, BlankFragment2.newInstance())
+                .commit()
+        }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, BlankFragment.newInstance())
+            .commit()
+    }
+}
